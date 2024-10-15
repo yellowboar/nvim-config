196450959dc59483be117a41bb95a5ddf5ee8887
yellowboar <bumaaka1@gmail.com>
2024-10-14 20:55:12 -0500

    random stuff

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -212,6 +212,7 @@ return {
       vim.list_extend(ensure_installed, {
         'eslint_d',
         'golangci-lint',
+        'jsonlint',
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
